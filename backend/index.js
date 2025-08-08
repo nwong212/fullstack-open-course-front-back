67b4f536b7cf1a989d14b86a6025e731428bdc34
Nathan <nathan@itsallwong.com>
2025-08-08 16:50:54 -0400

    remove extra static declaration

--- backend/index.js
+++ backend/index.js
@@ -4,8 +4,6 @@ const Note = require('./models/note')
 
 const app = express()
 
-app.use(express.static('dist'))
-
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
   console.log('Path:  ', request.path)
