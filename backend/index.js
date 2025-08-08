0a28760563b6bf1b24d29ecf4b318e6f5f961b7c
Nathan <nathan@itsallwong.com>
2025-08-08 16:29:37 -0400

    note-app

--- backend/index.js
+++ backend/index.js
@@ -1,123 +1,114 @@
+require('dotenv').config()
 const express = require('express')
-const morgan = require('morgan')
+const Note = require('./models/note')
 
 const app = express()
 
 app.use(express.static('dist'))
 
-let people = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
-app.use(express.json())
-
-app.use((request, response, next) => {
-    const oldJson = response.json
-    
-    response.json = (body) => {
-        response.locals.body = body
-        return oldJson.call(response, body)
-    }
-    
-    next()
-})
-
-morgan.token('bod', (req, res) => {
-    return JSON.stringify(res.locals.body)
-})
-
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :bod'))
-
-app.get('/', (request, response) => {
-    response.send('<h1>HELLO!</h1>')
-})
-
-app.get('/api/persons', (request, response) => {
-    response.json(people)
-})
-
-app.get('/info', (request, response) => {
-    const count = people.length
-
-    response.send(`<p>Phonebook has info for ${count} people.</p>
-        <p>${Date()}</p>
-        `)
-})
-
-app.get('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    const person = people.find(person => person.id === id)
-
-    if(person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
-})
-
-app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    people = people.filter(person => person.id !== id)
-
-    response.status(204).end()
-})
-
-const generateId = () => {
-    return String(Math.floor(Math.random() * 1255455))
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
 }
 
-app.post('/api/persons/', (request, response) => {
-    const body = request.body
+app.use(requestLogger)
+app.use(express.static('dist'))
+app.use(express.json())
 
-    if(!body.name) {
-        return response.status(400).json({
-            error:'name is missing'
-        })
-    }
-    if(!body.number) {
-        return response.status(400).json({
-            error:'number is missing'
-        })
-    }
-
-    if(Object.values(people.map(p => p.name)).find(p => p.toLowerCase() === body.name.toLowerCase())) {
-        return response.status(400).json({
-            error:'name must be unique'
-        })
-    }
-
-    const person = {
-        name: body.name,
-        number: body.number,
-        id: generateId()
-    }
-
-    people = people.concat(person)
-
-    response.json(person)
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
 })
 
-const PORT = process.env.PORT || 3001
+app.get('/api/notes', (request, response) => {
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
+})
 
+app.get('/api/notes/:id', (request, response) => {
+  Note.findById(request.params.id)
+    .then(note => {
+      if(note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
+app.post('/api/notes', (request, response, next) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({ error: 'content missing' })
+  }
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  })
+
+  note
+    .save()
+    .then(savedNote => {
+      response.json(savedNote)
+    })
+    .catch(error => next(error))
+})
+
+app.put('/api/notes/:id', (request, response, next) => {
+  const { content, important } = request.body
+
+  Note.findById(request.params.id)
+    .then(note => {
+      if (!note) {
+        return response.status(404).end()
+      }
+
+      note.content = content
+      note.important = important
+
+      return note.save().then((updatedNote) => {
+        response.json(updatedNote)
+      })
+    })
+    .catch(error => next(error))
+})
+
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
